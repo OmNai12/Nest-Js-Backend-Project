72c5e5805a61a5d920cc151555e87e23a170fb23
Om Nai <93648778+OmNai12@users.noreply.github.com>
2024-06-20 18:39:54 +0530

    Post request working and minor fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    // InjectRepository DI system needs the type of USER
+    // DI is not good ith genrics type so the decorator is there
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
+
+    /**
+     * 
+     * @param email 
+     * @param password 
+     * @returns 
+     */
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password });
+        return this.repo.save(user);
+    }
+}

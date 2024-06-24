c35d46699e445cd19cd2a47aa9c971e628bc907e
Om Nai <93648778+OmNai12@users.noreply.github.com>
2024-06-24 10:39:46 +0530

    User entity mapping for current user

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
 
 @Controller('reports')
 @UseGuards(AuthGuard)
@@ -10,7 +12,7 @@ export class ReportsController {
     constructor(private reportService: ReportsService) { }
 
     @Post()
-    async createReport(@Body() body: CreateReportDto) {
-        return await this.reportService.create(body);
+    async createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+        return await this.reportService.create(body, user);
     }
 }

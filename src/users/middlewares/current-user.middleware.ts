2f02dc0d14c00ae71d5623c54a2de312d6585459
Om Nai <93648778+OmNai12@users.noreply.github.com>
2024-06-24 17:57:18 +0530

    Middleware added for the current request

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,30 @@
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+import { UsersService } from "../users.service";
+import { User } from "../user.entity";
+
+// Now, here we added the currentUser property in the reqest object
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: User;
+        }
+    }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+
+    constructor(private userService: UsersService) { }
+
+    async use(req: Request, res: Response, next: NextFunction) {
+        const { userId } = req.session || {};
+        console.log(userId);
+        if (userId) {
+            const user = await this.userService.findOne(userId);
+            req.currentUser = user;
+        }
+        next();
+    }
+
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,20 +1,26 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
-import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
+// ! Old Implementation :- import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
+// ! Old Implementation :- import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, AuthService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CurrentUserInterceptor
-    }
+    // ! Old Implementation :- 
+    // {
+    //   provide: APP_INTERCEPTOR,
+    //   useClass: CurrentUserInterceptor
+    // }
   ]
 })
-export class UsersModule { }
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*');
+  }
+}

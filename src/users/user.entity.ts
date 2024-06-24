5dd364748514a9af790b6b7ec40d0ac36889ceb1
Om Nai <93648778+OmNai12@users.noreply.github.com>
2024-06-24 12:36:23 +0530

    Admin guard implemented

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -17,6 +17,9 @@ export class User {
   @OneToMany(() => Report, (report) => report.user)
   reports: Report[];
 
+  @Column({ default: true })
+  admin: boolean;
+
   @AfterInsert()
   logInsert() {
     console.log('User inserted', this.id);
